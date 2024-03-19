662432ac7431ad1b2f62c537cf34e9510c642b52
john220099 <148237962+John220099@users.noreply.github.com>
2024-03-19 10:07:53 +0100

    Add files via upload

---
+++ B.py
@@ -0,0 +1,96 @@
+import os
+import sys
+import json
+import pickle
+import shutil
+import subprocess
+import glob
+import tarfile
+import zipfile
+import gzip
+import bz2
+import csv
+import io
+import shutil
+import tarfile
+import zipfile
+"""
+import gzip"""
+import bz2
+import csv
+import io
+decompressor = bz2.BZ2Decompressor()
+#import gzip
+#import zipfile
+import tarfile
+import zipfile
+import gzip
+import tarfile
+import zipfile
+import bz2
+import gzip
+import tarfile
+import zipfile
+import bz2
+import gzip
+os.wait
+gzip.GzipFile
+import tarfile
+print("Hello World")
+#import gzip
+#import zipfile
+#import tarfile
+#import zipfile
+#import bz2
+#import gzip
+#import tarfile
+#import zipfile
+#import bz2
+#import gzip
+#import tarfile
+#import zipfile
+#import bz2
+#import gzip
+#import tarfile
+#import zipfile
+#import bz2
+print("Hello World")
+#import gzip
+#import zipfile
+"""
+import tarfile"""
+#import zipfile
+#import tarfile
+import zipfile
+import tarfile
+#import zipfile
+import tarfile
+#import zipfile
+#import tarfile
+#import zipfile
+#import tarfile
+#import zipfile
+#import tarfile
+#import zipfile
+#import tarfile
+#import zipfile
+#import tarfile
+#import zipfile
+#import tarfile
+#import zipfile
+#import tarfile 
+pickle.load
+pickle.loads
+pickle.load
+pickle.load
+pickle.load
+pickle.load
+pickle.load
+pickle.PickleBuffer
+pickle.PickleBuffer
+a = pickle.load
+pickle.load
+pickle.load
+pickle.load
+pickle.load
+print ("b")
