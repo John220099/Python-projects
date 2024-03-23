8f2b3affc3064fd71f0146b0d8b99c515f6e8678
john220099 <148237962+John220099@users.noreply.github.com>
2024-03-23 15:06:19 +0100

    B3 of the ?
    more py code more type more bad code

---
+++ b3.py
@@ -0,0 +1,18 @@
+import time
+#i am not goning to use this :)
+import pickle
+#also not usinng this ;)
+
+#this took 5 min to fix this 
+1 == 2
+2 == 1
+print(1)
+
+#print B1
+print("B1")
+
+time.sleep(1)
+print("time")
+time.sleep(99)
+print(";)")
+#man i love time
